95018a07d9ba5e785d1bc203a40a84a6be1722f8
Sir Felipe <felipe.dioliveira@outlook.com>
2019-05-11 12:51:45 -0300

    XPTO Dynamic Fragment Manager

--- FragmentsManager/app/src/main/java/br/com/fragmentsmanager/MainActivity.java
+++ FragmentsManager/app/src/main/java/br/com/fragmentsmanager/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.fragmentsmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exibirFragment1(View view) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, new MeuFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+
+    public void exibirFragment2(View view) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, new OutroFragment())
+                .addToBackStack(null)
+                .commit();
     }
 }
